954515ce5a5dfd75a3c4559c6c12f4addcda93d7
estherawww <esther@besquare.com.my>
2022-07-18 17:50:23 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+const ScoreContainer = () => {
+  return (
+    <div>
+      <div>
+        <div>Score</div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default ScoreContainer;
