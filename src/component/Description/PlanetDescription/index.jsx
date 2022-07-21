67f9416488d98fba0f745ebcbe62bf3955a7fa23
kathleen <kai.ling@besquare.com.my>
2022-07-21 11:12:22 +0800

    added more planet and adjusted some styling

--- src/component/Description/PlanetDescription/index.jsx
+++ src/component/Description/PlanetDescription/index.jsx
@@ -17,16 +17,58 @@ const PlanetDescription = () => {
       image: "https://nineplanets.org/wp-content/uploads/2020/03/sun.png",
     },
     {
+      planet_name: "Mercury",
+      planet_id: "PIA11245",
+      index: 1,
+      image: "https://nineplanets.org/wp-content/uploads/2020/03/mercury.png",
+    },
+    {
+      planet_name: "Venus",
+      planet_id: "PIA00257",
+      index: 2,
+      image: "https://nineplanets.org/wp-content/uploads/2020/03/venus.png",
+    },
+    {
       planet_name: "Earth",
       planet_id: "PIA18033",
-      index: 1,
+      index: 3,
       image: "https://nineplanets.org/wp-content/uploads/2020/03/earth.png",
+    },
+    {
+      planet_name: "Mars",
+      planet_id: "PIA02165",
+      index: 4,
+      image: "https://nineplanets.org/wp-content/uploads/2020/03/mars.png",
     },
     {
       planet_name: "Jupiter",
       planet_id: "PIA00343",
-      index: 2,
+      index: 5,
       image: "https://nineplanets.org/wp-content/uploads/2020/03/jupiter.png",
+    },
+    {
+      planet_name: "Saturn",
+      planet_id: "PIA05385",
+      index: 6,
+      image: "https://nineplanets.org/wp-content/uploads/2020/03/saturn.png",
+    },
+    {
+      planet_name: "Uranus",
+      planet_id: "PIA18182",
+      index: 7,
+      image: "https://nineplanets.org/wp-content/uploads/2020/03/uranus.png",
+    },
+    {
+      planet_name: "Neptune",
+      planet_id: "PIA01492",
+      index: 8,
+      image: "https://nineplanets.org/wp-content/uploads/2020/03/neptune.png",
+    },
+    {
+      planet_name: "Pluto",
+      planet_id: "PIA20291",
+      index: 9,
+      image: "https://nineplanets.org/wp-content/uploads/2020/03/pluto.png",
     },
   ];
 
