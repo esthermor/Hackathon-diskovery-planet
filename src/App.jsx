49a831e74a66101004de7254bc52308ae12d119c
kathleen <kai.ling@besquare.com.my>
2022-07-21 13:59:18 +0800

    update on start quizz component

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,36 @@
 import React from "react";
 import "./App.css";
 import Header from "./component/Header";
-import MiniQuizz from "./component/MiniQuizz";
+import MiniQuizz from "./component/MiniQuizz/MiniQuizzPage";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import PlanetDescription from "./component/Description/PlanetDescription";
 import NasaPhoto from "./component/POTD/nasaphoto";
+import StartQuizz from "./component/MiniQuizz/StartQuizz";
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" exact element={<PlanetDescription />} />
-        <Route path="/miniquizz" element={<MiniQuiz/>} />
-        <Route path="/forum" element={<></>} />
-        <Route path="/potd" element={<NasaPhoto />} />
-      </Routes>
-    </Router>
-  )
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route
+            path="/"
+            exact
+            element={
+              <>
+                <PlanetDescription />
+                <StartQuizz />
+                <NasaPhoto />
+              </>
+            }
+          />
+          <Route path="/miniquizz" element={<MiniQuizz />} />
+          <Route path="/forum" element={<></>} />
+          <Route path="/potd" element={<NasaPhoto />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 export default App;
 //<Route path="/nasaphoto" element={<NasaPhoto />}></Route>;
